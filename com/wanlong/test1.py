94b8d47a26669865a8bae91ba3eefba766bb86dc
wanlong <wanlong3@jd.com>
2019-02-14 18:05:00 +0800

    Issue1:增加部分笔记

--- com/wanlong/test1.py
+++ com/wanlong/test1.py
@@ -1,4 +1,12 @@
-print(100+200+300)
+print(100 + 200 + 300)
 print("hello,world")
 
-print('The quick brown fox','jumps over','the fly')
+print('The quick brown fox', 'jumps over', 'the fly')
+# name = input("1234")
+# name
+
+a = 100
+if a > 10:
+    print(1202)
+else:
+    print(2222)
